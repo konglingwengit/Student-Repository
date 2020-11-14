d0496cd7a8fba731cde100eef75f737baae12ae5
konglingwengit <74449120+konglingwengit@users.noreply.github.com>
2020-11-14 14:38:03 +0500

    Student respository
    HW09 Student respository

---
+++ HW09/Student_Repository_Test_Lingwen_Kong.py
@@ -0,0 +1,22 @@
+import unittest
+from HW09_Lingwen_Kong import Repository
+import os
+
+class TestRepository(unittest.TestCase):
+    def test_repository(self):
+        # pass directory path with all files
+        test_obj = Repository(os.getcwd()+'/hw9/')
+        # validation
+        self.assertEqual(len(test_obj.instructors), 6)
+        self.assertEqual(len(test_obj.students), 10)
+        self.assertEqual(test_obj.instructors.get('98765').name, 'Einstein, A')
+        self.assertEqual(test_obj.instructors.get('98765').dept, 'SFEN')
+        self.assertEqual(test_obj.instructors.get('98765').course, {'SSW 567': 4, 'SSW 540': 3})
+        self.assertEqual(test_obj.students.get('11788').name, 'Fuller, E')
+        self.assertEqual(test_obj.students.get('11788').major, 'SYEN')
+        self.assertEqual(test_obj.students.get('11788').course['SSW 540'], 'A')
+        self.assertEqual(test_obj.students.get('11788').course['SSW 541'], '')
+
+
+if __name__ == '__main__':
+    unittest.main()
